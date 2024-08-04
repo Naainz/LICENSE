9f508279081c52abc41af94d9ca4c89dbc9474f4
Stefan <67121265+Naainz@users.noreply.github.com>
2024-08-04 15:39:28 +0300

    Add theme toggle and initial layout

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+import dotenv from 'dotenv';
+import vercel from '@astrojs/vercel/serverless';
+import tailwind from "@astrojs/tailwind";
+dotenv.config();
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+  vite: {
+    css: {
+      preprocessorOptions: {
+        css: {
+          additionalData: `@import "./src/styles/global.css";`
+        }
+      }
+    }
+  },
+  integrations: [tailwind()]
+});
