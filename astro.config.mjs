c5f693bb3334b21a45136845e4c707b950735307
Stefan <67121265+Naainz@users.noreply.github.com>
2024-08-06 17:33:20 +0300

    Attempt #4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import dotenv from 'dotenv';
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
-import vercelServerless from "@astrojs/vercel/serverless";
 dotenv.config();
-
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercelServerless(),
+  adapter: vercel(),
   vite: {
     css: {
       preprocessorOptions: {
