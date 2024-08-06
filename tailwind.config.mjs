e24809e557013bdf5e929422866fbc78c77782c4
Stefan <67121265+Naainz@users.noreply.github.com>
2024-08-06 16:33:09 +0300

    Retry

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+	content: [
+	  './src/**/*.{js,ts,jsx,tsx,astro}',
+	],
+	theme: {
+	  extend: {
+		transitionProperty: {
+		  'colors': 'background-color, border-color, color, fill, stroke',
+		},
+	  },
+	},
+	plugins: [],
+	darkMode: 'class', // Enable class-based dark mode
+  }
+  
