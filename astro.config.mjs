1108e5800c3aa0985a7caf8990cb21bca7170960
Stefan <67121265+Naainz@users.noreply.github.com>
2024-08-06 17:13:47 +0300

    Vercel to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import dotenv from 'dotenv';
-import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
 dotenv.config();
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
   vite: {
     css: {
       preprocessorOptions: {
